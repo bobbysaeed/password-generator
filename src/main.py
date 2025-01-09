11c8e8368790a58a2b66d9c649164ded7095d1e6
saeed <saeedbabakan10@gmail.com>
2025-01-09 09:58:29 -0800

    add pin password generator class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
+import random
+import string
 from abc import ABC, abstractmethod
+
 class PasswordGenerator(ABC):
     @abstractmethod
     def generate(self):
         pass
+
+
+class PinCodeGenerator(PasswordGenerator):
+    """
+    Class to generate a numeric pin code.
+    """
+    def __init__(self, length: int = 6):
+        self.length: int = length
+
+    def generate(self) -> str:
+        """
+        Generate a numeric pin code.
+        """
+        return ''.join(random.choice(string.digits) for _ in range(self.length))
